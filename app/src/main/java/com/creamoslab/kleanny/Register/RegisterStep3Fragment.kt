a7f3b5085daf2924dd32c7b251f018371ee05674
Miguel Pereyra <mapc.1993@hotmail.com>
2019-08-17 20:09:33 -0500

    Se añadieron pantallas de registro
    Aquí se añadió el esqueleto de las pantallas de registro. El tema para que utilicen los botones y se añadieron los fondos de letra que se usarán

---
+++ app/src/main/java/com/creamoslab/kleanny/Register/RegisterStep3Fragment.kt
@@ -0,0 +1,22 @@
+package com.creamoslab.kleanny.Register
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.creamoslab.kleanny.R
+
+class RegisterStep3Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_step3, container, false)
+    }
+}
