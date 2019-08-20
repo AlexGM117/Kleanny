1b19bc2a86b66494d331255b87722e01e0fd092d
Mattencio <mapc.1993@hotmail.com>
2019-08-20 10:33:37 -0500

    Login conectado con MainActivity

--- app/src/main/java/com/creamoslab/kleanny/MainActivity.kt
+++ app/src/main/java/com/creamoslab/kleanny/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadLoginFragment()
+    }
+
+    private fun loadLoginFragment() {
+        val loginFragment = LoginFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, loginFragment).commit()
     }
 }
--- app/src/main/java/com/creamoslab/kleanny/SplashActivity.kt
+++ app/src/main/java/com/creamoslab/kleanny/SplashActivity.kt
@@ -1,15 +1,24 @@
 package com.creamoslab.kleanny
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    private val mTimerToLoadLogin = 2000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //        setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            loadLoginActivity()
+        }, mTimerToLoadLogin)
+    }
+
+    private fun loadLoginActivity() {
         startActivity(Intent(this, MainActivity::class.java))
     }
 }
