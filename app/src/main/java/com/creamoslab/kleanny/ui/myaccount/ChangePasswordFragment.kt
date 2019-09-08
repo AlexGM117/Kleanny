ed551171861bc173f565450d8808724420a9c5df
Miguel Pereyra <mapc.1993@hotmail.com>
2019-09-08 15:20:28 -0500

    Validación de contraseñas en Cambio de contraseña, añadido

--- app/src/main/java/com/creamoslab/kleanny/ui/myaccount/ChangePasswordFragment.kt
+++ app/src/main/java/com/creamoslab/kleanny/ui/myaccount/ChangePasswordFragment.kt
@@ -1,19 +1,23 @@
 package com.creamoslab.kleanny.ui.myaccount
 
-
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.creamoslab.kleanny.R
 import com.creamoslab.kleanny.data.manager.KleannyPreferences
 import com.creamoslab.kleanny.ui.base.AbstractFragment
 import kotlinx.android.synthetic.main.fragment_change_password.*
+import kotlinx.android.synthetic.main.fragment_change_password.inputCurrentPassword
 
 class ChangePasswordFragment : AbstractFragment() {
 
+    private var mArePasswordsValid: Boolean = false
     private val mViewModel: MyAccountViewModel by lazy {
         ViewModelProviders.of(this).get(MyAccountViewModel::class.java)
     }
@@ -29,15 +33,65 @@ class ChangePasswordFragment : AbstractFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setFieldsListeners()
+
         buttonAccept.setOnClickListener {
-            showProgressDialog(buttonAccept)
-            mViewModel.callChangePassword(KleannyPreferences().email!!, inputCurrentPassword.editText?.text.toString(),
-                inputNewPassword.editText?.text.toString()).observe(this, Observer {
-                it?.let {
-                    hideProgressDialog(buttonAccept)
-                    showBottomMessage(it.message)
-                }
-            })
+            if (validatePasswords()) {
+                showProgressDialog(buttonAccept)
+                mViewModel.callChangePassword(
+                    KleannyPreferences().email!!, inputCurrentPassword.editText?.text.toString(),
+                    inputNewPassword.editText?.text.toString()
+                ).observe(this, Observer {
+                    it?.let {
+                        hideProgressDialog(buttonAccept)
+                        showBottomMessage(it.message)
+                    }
+                })
+            }
         }
+    }
+
+    /**
+     * Si las contraseñas no coinciden, se alerta al usuario. Además este método te regresa
+     * si las contaseñas son válidas
+     */
+    private fun validatePasswords(): Boolean {
+        if (!mArePasswordsValid) {
+            editText_new_password.error = getString(R.string.error_password)
+            inputNewPassword.isPasswordVisibilityToggleEnabled = false
+        }
+        return  mArePasswordsValid
+    }
+
+    private fun setFieldsListeners() {
+        editText_new_password.afterTextChanged { mViewModel.password1 = it }
+        editText_new_password.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus && !inputNewPassword.isPasswordVisibilityToggleEnabled) {
+                inputNewPassword.isPasswordVisibilityToggleEnabled = true
+            }
+
+            if (hasFocus && editText_new_password.error != null) {
+                editText_new_password.setError(getString(R.string.error_password), null)
+            }
+        }
+        editText_repeat_new_password.afterTextChanged { mViewModel.password2 = it}
+
+        mViewModel.arePasswordsValid.observe(this, Observer { valid ->
+            mArePasswordsValid = valid
+        })
+    }
+
+    private fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                afterTextChanged.invoke(editable.toString())
+            }
+        })
     }
 }
--- app/src/main/java/com/creamoslab/kleanny/ui/myaccount/MyAccountViewModel.kt
+++ app/src/main/java/com/creamoslab/kleanny/ui/myaccount/MyAccountViewModel.kt
@@ -5,9 +5,32 @@ import androidx.lifecycle.MutableLiveData
 import com.creamoslab.kleanny.data.model.UsuarioCredenciales
 import com.creamoslab.kleanny.data.remote.response.BaseResponse
 import com.creamoslab.kleanny.ui.base.BaseViewModel
+import com.creamoslab.kleanny.ui.register.validator.FieldsValidator
 import kotlinx.coroutines.launch
 
 class MyAccountViewModel: BaseViewModel() {
+
+    private val _arePasswordsValid = MutableLiveData<Boolean>()
+
+    val arePasswordsValid: LiveData<Boolean>
+        get() = _arePasswordsValid
+
+    var password1 = ""
+        set(value) {
+            field = value
+            validatePassword()
+        }
+
+    var password2 = ""
+        set(value) {
+            field = value
+            validatePassword()
+        }
+
+    private fun validatePassword() {
+        val isValid = FieldsValidator.arePasswordsValid(password1, password2)
+        _arePasswordsValid.postValue(isValid)
+    }
 
     fun callChangePassword(email: String, password: String, newPassword: String) :LiveData<BaseResponse<Nothing>> {
         val result = MutableLiveData<BaseResponse<Nothing>>()
